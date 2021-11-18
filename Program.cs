08a898e7aefc2d3b2af65600ae564b8d0890c802
MouraRei <reinaldomoura.contato@outlook.com>
2021-11-18 16:40:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿    for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+    {
+        for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+        {
+            Console.Write("*");
+            if (contadorColuna >= contadorLinha)
+                break;
+        }
+        Console.WriteLine();
+    }     
